274cba5715fdf61f9c756abd745416daca0a972f
tonytoes <159160091+tonytoes@users.noreply.github.com>
2026-02-25 22:39:13 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+file_path ='./dataset/flood-prediction.csv'
+
+df = pd.read_csv(file_path, encoding='ascii', delimiter=',')
+
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+
+df.head()
